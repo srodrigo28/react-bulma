86fefbe49781b180fc2002f47133a2d45bfea101
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-07-05 12:38:56 -0300

    update formulario com opção de status

--- src/pages/Servicos/index.tsx
+++ src/pages/Servicos/index.tsx
@@ -4,11 +4,12 @@ export function Servicos() {
     const [desc, setDesc] = useState("")
     const [preco, setPreco] = useState("")
     const [data, setData] = useState("")
+    const [status, setStatus] = useState("")
 
     const submit = (e: any) => {
         e.preventDefault()
         const servico = {
-            desc, preco, data
+            desc, preco, data, status
         }
         console.log(servico)
     }
@@ -16,9 +17,13 @@ export function Servicos() {
         alert('Voltando ...')
     }
     return (
-        <div className="container column is-10">
-            <div className="header"><h1>Serviços</h1></div>
-            <div className="conteudo">
+        <div className="container column is-12">
+
+            <div className="header">
+                <h1>Serviços</h1>
+            </div>
+            
+            <div className="formulario">
                 <form>
                     <input
                         type="text"
@@ -32,6 +37,11 @@ export function Servicos() {
                         placeholder="Preço"
                         onChange={ e => setPreco(e.target.value)}
                     />
+                    <select value={status} onChange={ e => setStatus(e.target.value)}>
+                        <option value="Pendente">Pendente</option>
+                        <option value="Aguardando Aprovação">Aguardando Aprovação</option>
+                        <option value="Aprovado Concluído">Aprovado Concluído</option>
+                    </select>
                     <input
                         type="date"
                         value={data}
@@ -41,6 +51,28 @@ export function Servicos() {
                     <button onClick={submit}>Cadastrar</button>
                     <button onClick={voltar}>Voltar</button>
                 </form>
+            
+            </div>
+            
+            <div className="lista-produtos">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Data</th>
+                            <th>Descrição</th>
+                            <th>Valor</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>20-10-2023</td>
+                            <td>Instalar Câmeras</td>
+                            <td>200,00</td>
+                            <td>Pendente</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     )
